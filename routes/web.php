2a3f8ba13ce6158e791e121b78d69c0eab988f68
Acedev-1907 <thanhqui19072000@gmail.com>
2024-08-11 22:10:05 +0700

    install and setting Vue

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/app/{any}', function () {
     return view('welcome');
 });
+
 
 Route::get('/check_email/{token}', [AuthController::class, 'validEmail'])->name('validEmail');
