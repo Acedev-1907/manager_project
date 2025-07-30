926b447edc64b6f76a2360573e7a58caa3489736
Qui Nguyen <thanhqui19072000@gmail.com>
2025-07-31 02:30:16 +0700

    fix loading page (z-index)

--- resources/js/src/components/LoadingPage.vue
+++ resources/js/src/components/LoadingPage.vue
@@ -29,7 +29,7 @@ const props = defineProps({
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: 9999;
+    z-index: 99;
     background: rgba(255, 255, 255, 0.95);
     display: flex;
     align-items: center;
