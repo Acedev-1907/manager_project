9359b3d8347a988eb8717f7f1bc8af623059ee38
Qui Nguyen <thanhqui19072000@gmail.com>
2025-08-09 01:56:38 +0700

    Comment out member store implementation in MemberStore.ts for future reference

--- resources/js/src/pages/app/member/store/MemberStore.ts
+++ resources/js/src/pages/app/member/store/MemberStore.ts
@@ -1,23 +1,23 @@
-import { defineStore } from "pinia";
-import { MemberInputType } from "../actions/createMember";
+// import { defineStore } from "pinia";
+// import { MemberInputType } from "../actions/createMember";
 
-const userMemberStore = defineStore("member", {
-  state: () => ({
-    memberInput: {} as MemberInputType,
-    edit: false,
-    memberListCache: null as any, // cache member list
-    lastFetched: null as number | null, // Last fetch time
-  }),
-  actions: {
-    setMemberListCache(data: any) {
-      this.memberListCache = data;
-      this.lastFetched = Date.now();
-    },
-    clearMemberListCache() {
-      this.memberListCache = null;
-      this.lastFetched = null;
-    },
-  },
-});
+// const userMemberStore = defineStore("member", {
+//   state: () => ({
+//     memberInput: {} as MemberInputType,
+//     edit: false,
+//     memberListCache: null as any, // cache member list
+//     lastFetched: null as number | null, // Last fetch time
+//   }),
+//   actions: {
+//     setMemberListCache(data: any) {
+//       this.memberListCache = data;
+//       this.lastFetched = Date.now();
+//     },
+//     clearMemberListCache() {
+//       this.memberListCache = null;
+//       this.lastFetched = null;
+//     },
+//   },
+// });
 
-export const memberStore = userMemberStore();
+// export const memberStore = userMemberStore();
