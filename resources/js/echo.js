10bc53eb8b9a9da75d3f24d96c85d49544e78886
Acedev-1907 <thanhqui19072000@gmail.com>
2025-12-16 10:50:21 +0700

    up code

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,40 +3,16 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-function getCurrentToken() {
-    const userDataRaw = localStorage.getItem('userData');
-    const token = userDataRaw ? JSON.parse(userDataRaw)?.token : '';
-
-    return token;
-}
-
 export function initEcho() {
-    if (window.Echo) {
-        window.Echo.disconnect();
-        window.Echo = null;
-    }
-
-    const token = getCurrentToken();
-
-    // Avoid Permissions-Policy warning: skip Pusher unload listener
-    if (Pusher?.Runtime && typeof Pusher.Runtime.addUnloadListener === 'function') {
-        Pusher.Runtime.addUnloadListener = () => {};
-    }
-
     window.Echo = new Echo({
         broadcaster: 'reverb',
         key: import.meta.env.VITE_REVERB_APP_KEY,
-        wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
-        wsPort: import.meta.env.VITE_REVERB_PORT || 80,
-        wssPort: import.meta.env.VITE_REVERB_PORT || 443,
-        forceTLS: (import.meta.env.VITE_REVERB_SCHEME || 'http') === 'https',
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+	wsPath: '/ws',
         enabledTransports: ['ws', 'wss'],
-        auth: {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                Accept: 'application/json'
-            }
-        }
     });
 }
 
